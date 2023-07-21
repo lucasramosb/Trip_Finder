fde8db3d6fdc6918c8a2dcaeee64866ebe613efe
Lucas <lucasramos1123.lr29@gmail.com>
2023-07-21 13:30:40 -0300

    initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Trips from "./components/trips";
+
+export const metadata = {
+    title: "about"
+}
+
+const Page = () => {
+    return ( 
+        <div><Trips></Trips></div>
+    );
+}
+ 
+export default Page;
