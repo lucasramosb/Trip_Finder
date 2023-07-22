60618d3e862093dac4b0f897fd1e4a2f61eddda4
Lucas <lucasramos1123.lr29@gmail.com>
2023-07-22 14:06:10 -0300

    feat: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+"use client"
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function Home() {
+
+
   return (
-    <h1>Home Page</h1>
+    <div>
+
+    </div>
   )
 }
