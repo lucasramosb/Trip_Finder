a5496ea699b9e1efa6ee543bfa3ee9e07e103199
Lucas <lucasramos1123.lr29@gmail.com>
2023-07-25 21:24:25 -0300

    feat: add recommended trips

---
+++ src/app/components/RecommendedTrips.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import TripItem from '@/components/TripItem';
+import { Trip } from '@prisma/client';
+
+const RecommendedTrips = async () => {
+
+    const data = await fetch('http://localhost:3000/hello').then(res => res.json());
+
+    return ( 
+        <div className="container mx-auto p-5">
+            <div className="flex items-center">
+                <div className="w-full h-[2px] bg-grayPrimary"></div>
+                <h2 className="font-medium text-grayPrimary whitespace-nowrap p-1">Destinos recomendadas</h2>
+                <div className="w-full h-[2px] bg-grayPrimary"></div>
+            </div>
+
+            <div className="flex flex-col items-center mt-5 gap-5">
+                {data.map((trip: Trip) => (
+                    <TripItem key={trip.id} trip={trip} />
+                ))}
+            </div>
+        </div>    
+    );
+}
+ 
+export default RecommendedTrips;
