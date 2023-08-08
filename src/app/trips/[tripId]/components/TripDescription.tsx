a22741b9e11481332304c91d38464785d89dbac9
Lucas <lucasramos1123.lr29@gmail.com>
2023-08-07 21:13:39 -0300

    style: add description text

--- src/app/trips/[tripId]/components/TripDescription.tsx
+++ src/app/trips/[tripId]/components/TripDescription.tsx
@@ -9,7 +9,7 @@ const TripDescription = ({description}: TripDescriptionProps) => {
     return ( 
         <div className="flex flex-col p-5">
             <h2 className="font-semibold text-primaryDarker">Sobre a viagem</h2>
-            <p className='text-xs leading-5 text-primaryDarker mt-4'>{description}</p>
+            <p className='text-xs leading-5 text-primaryDarker mt-3'>{description}</p>
         </div>
     );
 }
