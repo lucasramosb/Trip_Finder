d732726d0db08755aa4b79a5d46d14898dcd7041
Lucas <lucasramos1123.lr29@gmail.com>
2023-09-07 20:33:15 -0300

    feat: add my-trips buttom to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,7 +49,14 @@ const Header = () => {
 
                     {menuIsOpen && (
                         <div className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
-                            <button className='text-primary text-sm font-semibold '  onClick={handleLogoutClick}>
+                            <Link href='/my-trips' >
+                                <button className='text-primary pb-2 border-b border-grayLighter border-solid text-sm font-semibold '  onClick={handleLogoutClick}>
+                                    Minhas viagens 
+                                </button>
+                            </Link>
+
+
+                            <button className='text-primary pt-2 text-sm font-semibold '  onClick={handleLogoutClick}>
                                 Logout 
                             </button>
                         </div>
