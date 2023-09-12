9f982fb34dfb85eafc60a3f6be5f5255335dde2f
Lucas <lucasramos1123.lr29@gmail.com>
2023-09-12 10:52:47 -0300

    style: adpat search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -26,9 +26,9 @@ const Trips = async () => {
 
     return ( 
         <div className="container mx-auto flex flex-col p-5 items-center">
-            <h1 className="text-primaryDarker font-semibold text-xl">Viagens Encontradas</h1>
-            <h2 className="text-grayPrimary font-medium mb-5">{trips.length > 0 ? "Listamos as melhores viagens pra você" : "Não encontramos nada nos seus prametros"}</h2>
-            <div className="flex flex-col gap-4">
+            <h1 className="text-primaryDarker font-semibold text-xl lg:text-[2.5rem]">Viagens Encontradas</h1>
+            <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6">{trips.length > 0 ? "Listamos as melhores viagens pra você" : "Não encontramos nada nos seus prametros"}</h2>
+            <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg-mt-6 lg:pb-16">
                 {trips?.map((trip) => <TripItem key={trip.id} trip={trip}/>)}
             </div>
         </div>
